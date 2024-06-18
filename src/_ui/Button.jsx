c768011839248583f220ea046280ebba016571a7
Aman kumar <amank0259@gmail.com>
2024-06-18 11:19:07 +0530

    button UI

---
+++ src/_ui/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Button({ text, className }) {
+    return (
+        <>
+            <button className={`min-w-10 h-10 w-10 hover:bg-blue-50 active:bg-blue-100 active:duration-100 rounded-full flex items-center justify-center self-center ${className}`}>
+                {text}
+            </button>
+        </>
+    );
+}
+
+export default Button;
