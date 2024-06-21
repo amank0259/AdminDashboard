53696d9b0d5982d0853070dd77b810b3dab7f49b
Aman kumar <amank0259@gmail.com>
2024-06-21 08:52:14 +0530

    take onClick prop

--- src/_ui/Button.jsx
+++ src/_ui/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Button({ text, className }) {
+function Button({ text, className, onClick }) {
     return (
         <>
-            <button className={`min-w-10 h-10 w-10 hover:bg-blue-100 active:bg-blue-200 active:duration-100 rounded-full flex items-center justify-center self-center ${className}`}>
+            <button onClick={onClick} className={`min-w-10 h-10 w-10 hover:bg-blue-100 active:bg-blue-200 active:duration-100 rounded-full flex items-center justify-center self-center ${className}`}>
                 {text}
             </button>
         </>
