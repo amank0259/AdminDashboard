1969d5303a7b13c15874e9e8c7c1187ea81f67c1
Aman kumar <amank0259@gmail.com>
2024-06-22 09:15:29 +0530

    fix the searchinput size

--- src/components/Header/SearchBox/index.jsx
+++ src/components/Header/SearchBox/index.jsx
@@ -5,7 +5,7 @@ const SearchBox = () => {
     return (
         <div className='w-[400px] h-10 bg-blue-100 rounded-lg relative flex items-center py-1 px-3'>
             <IoSearchOutline className='mx-2 text-black' size={20} />
-            <input type="text" className='bg-transparent border-[0px] outline-none px-1 text-[14px] text-black font-semibold' placeholder='Search here...' />
+            <input type="text" className='bg-transparent border-[0px] outline-none w-full px-1 text-[14px] text-black font-semibold' placeholder='Search here...' />
         </div>
     )
 }
